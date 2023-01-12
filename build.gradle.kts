6be0a878b3fa43842ed9cd15472a795f4eb3d07d
AxelBlaz3 <karthikgaddam4@gmail.com>
2023-01-11 23:03:30 -0500

    Add google maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 buildscript {
+    repositories {
+        google()
+    }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "7.3.1" apply false
