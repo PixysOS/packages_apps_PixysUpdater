e7de8a938bbd0cdc297ec682a8b0a34866afcb92
AxelBlaz3 <karthikgaddam4@gmail.com>
2023-01-18 18:08:57 -0500

    Introduce Updates feature

---
+++ app/src/main/java/org/pixysos/updater/feature/updates/UpdatesViewModel.kt
@@ -0,0 +1,19 @@
+package org.pixysos.updater.feature.updates
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.pixysos.updater.data.model.UpdatePackage
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
+
+sealed interface HomeUiState {
+    object Loading : HomeUiState
+    data class Success(val updatePackage: UpdatePackage) : HomeUiState
+    data class Error(val error: Throwable? = null) : HomeUiState
+}
