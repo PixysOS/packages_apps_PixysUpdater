4cebfe5f5dbd950614e588251e9dd8955964776d
AxelBlaz3 <karthikgaddam4@gmail.com>
2023-11-05 16:44:28 -0500

    Use edgeToEdge API and optimize imports

--- app/src/main/java/org/pixysos/updater/MainActivity.kt
+++ app/src/main/java/org/pixysos/updater/MainActivity.kt
@@ -3,15 +3,11 @@ package org.pixysos.updater
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import org.pixysos.updater.core.designsystem.theme.UpdaterTheme
 import org.pixysos.updater.feature.updates.UpdatesScreen
@@ -22,20 +18,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         // Turn off decor fitting system windows.
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        // This also sets up initial status bar and navigation bar styles.
+        enableEdgeToEdge()
 
         setContent {
-            val systemUiController = rememberSystemUiController()
-            val darkTheme = isSystemInDarkTheme()
-
-            DisposableEffect(key1 = systemUiController, key2 = darkTheme) {
-                systemUiController.setSystemBarsColor(
-                    color = Color.Transparent,
-                    darkIcons = !darkTheme
-                )
-                onDispose { }
-            }
-
             UpdaterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
